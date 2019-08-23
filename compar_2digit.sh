2a8753e5e75f454957dd141f5837ad64afd5595f
root <root@ip-172-31-45-128.us-east-2.compute.internal>
2019-08-23 23:45:04 +0000

    file name changed

--- compar_2digit.sh
+++ compar_2digit.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # This script compares 2 numbers and will display the relation of those numbers
-read -P "Enter the first number:" first
+echo "enter two numbers"
+read first second
+#read -P "Enter the first number:" first
 echo "first number is $first"
-read -P "Enter the second number:" second
+#read -P "Enter the second number:" second
 echo "second number is $second"
-if [$first \> $second];
-then 
+if ((first > second))
+then
   echo "first number $first is greater than second number $second"
 
 else
