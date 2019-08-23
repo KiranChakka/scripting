e256dd0955b0ca7433c39bbbbbd6b00446487bd8
kiran.chakka <kiran.chakka@gmail.com>
2019-08-24 00:13:01 +0100

    updated file arithemetic example

--- arithmetic_example.sh
+++ arithmetic_example.sh
@@ -2,5 +2,6 @@
 sec_permin=60
 min_perhr=60
 hrs_perday=24
-echo "Number of seconds in a day is $[$sec_permin*$min_perhr*$hrs_perday]"
+sec_min_hr_day=$[$sec_permin*$min_perhr*$hrs_perday]
+echo "Number of seconds in a day is $sec_min_hr_day"
 
