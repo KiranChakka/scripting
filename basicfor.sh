c2ccc5520a8638d2cdc9d0e06ec5412f5e50e2e1
kiran.chakka <kiran.chakka@gmail.com>
2019-08-24 11:59:54 +0100

    added list file

--- basicfor.sh
+++ basicfor.sh
@@ -2,5 +2,6 @@
 #this program is for understanding basic for loop
 #for HOST in host1 host2 host3; do
 for HOST in host{1,2,3,4,5}; do
+#for HOST in host{1,,10}; do
 echo $HOST
 done
---
+++ list_files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#this program is for listing files in a specific path
+for file in 'll /home/ec2-user/scripting'; do
+echo $file
+done
