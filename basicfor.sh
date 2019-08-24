62cb81bf597dc915f1f2a8674dbc000ae52ff422
root <root@ip-172-31-45-128.us-east-2.compute.internal>
2019-08-24 10:50:36 +0000

    updated for

--- basicfor.sh
+++ basicfor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #this program is for understanding basic for loop
-for HOST in host1 host2 host3; do
-# for HOST in host{1,2,3,4,5}
+#for HOST in host1 host2 host3; do
+for HOST in host{1,2,3,4,5}; do
 echo $HOST
 done
